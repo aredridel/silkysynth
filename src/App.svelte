b3ee786e08961a094a39a57b25cf24fb4f6b3369
Aria Stewart <aredridel@dinhe.net>
2019-09-13 22:38:42 -0400

    Make it so

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	import webaudio from "webaudio";
+	import { spring } from 'svelte/motion';
+
+	const tau = Math.PI * 2;
+	let frequency = 555
+	let volume = 0;
+
+	function sine(time, i) {
+		return Math.sin(time * tau * frequency) * volume
+	}
+
+	const channel = webaudio(sine);
+
+	channel.play();
+</script>
+
+<div style="position: absolute; right: 1em;">
+	<label>
+		<h3>volume ({volume})</h3>
+		<input bind:value={volume} type="range" min="0" max="1" step="0.01">
+	</label>
+	<label>
+		<h3>frequency ({frequency})</h3>
+		<input bind:value={frequency} type="range" min="20" max="2000" step="0.01">
+	</label>
+</div>
