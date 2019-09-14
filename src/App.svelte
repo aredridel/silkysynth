73ec6d187a67c27f1955ebd9429ddf5d854c93cc
Aria Stewart <aredridel@dinhe.net>
2019-09-13 22:51:38 -0400

    square waves

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,17 @@
 	const tau = Math.PI * 2;
 	let frequency = 555
 	let volume = 0;
+	let type = 'sine';
 
-	function sine(time, i) {
-		return Math.sin(time * tau * frequency) * volume
+	function wave(time, i) {
+		if (type == 'sine') {
+			return Math.sin(time * tau * frequency) * volume
+		} else if (type == 'square') {
+			return (Math.sin(time * tau * frequency) > 0 ? 1 : -1) * volume
+		}
 	}
 
-	const channel = webaudio(sine);
+	const channel = webaudio(wave);
 
 	channel.play();
 </script>
@@ -24,4 +29,11 @@
 		<h3>frequency ({frequency})</h3>
 		<input bind:value={frequency} type="range" min="20" max="2000" step="0.01">
 	</label>
+	<fieldset>
+		<legend>
+			<h3>type ({type})</h3>
+		</legend>
+		<label><input type=radio bind:group={type} value='sine'> Sine</label>
+		<label><input type=radio bind:group={type} value='square'> Square</label>
+	</fieldset>
 </div>
